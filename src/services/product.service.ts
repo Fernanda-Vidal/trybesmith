6c8864db6c6e869131b58d42080dfa892005c177
Fernanda Vidal <fevidal.dev@gmail.com>
2022-10-06 17:06:20 -0300

    feat: product service.

---
+++ src/services/product.service.ts
@@ -0,0 +1,16 @@
+import IProduct from '../interfaces/product.interface';
+import connection from '../models/connection';
+import ProductModel from '../models/product.model';
+
+export default class ProductService {
+  private model: ProductModel;
+
+  constructor() {
+    this.model = new ProductModel(connection);
+  }
+
+  public async create(product: IProduct): Promise<IProduct> {
+    const result = await this.model.createProduct(product);
+    return result;
+  }
+}
