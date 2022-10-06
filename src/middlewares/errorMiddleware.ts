8df0decaf6aac8eb12a4005603ddb45c91498d29
Fernanda Vidal <fevidal.dev@gmail.com>
2022-10-06 17:05:26 -0300

    feat: errorMiddleware.

---
+++ src/middlewares/errorMiddleware.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from 'express';
+
+function errorMiddleware(err: Error, _req: Request, res: Response, next: NextFunction) {
+  const { name, message, details } = err as any;
+
+  switch (name) {
+    case 'ValidationError':
+      res.status(400).json({ message: details[0].message });
+      break;
+    case 'NotFoundError':
+      res.status(404).json({ message });
+      break;
+    case 'conflictError':
+      res.status(409).json({ message });
+      break;
+    default:
+      console.log(err);
+      res.sendStatus(500);
+  }
+  next();
+}
+
+export default errorMiddleware;
