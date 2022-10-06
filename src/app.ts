86ee33de7f59560b361940f59510a594f0a4355e
Fernanda Vidal <fevidal.dev@gmail.com>
2022-10-06 20:19:53 -0300

    feat: user route.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import errorMiddleware from './middlewares/errorMiddleware';
 import ProductRoutes from './routes/product.routes';
+import UserRoutes from './routes/user.routes';
 
 const app = express();
 
 app.use(express.json());
 app.use('/products', ProductRoutes);
+app.use('/users', UserRoutes);
 
 app.use(errorMiddleware);
 
