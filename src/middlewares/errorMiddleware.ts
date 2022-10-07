1445c323b13326cde747169f81b7005b2eb2f5ed
Fernanda Vidal <fevidal.dev@gmail.com>
2022-10-07 16:46:12 -0300

    fix: error middleware.

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -1,21 +1,22 @@
 import { Request, Response, NextFunction } from 'express';
+import StatusCodes from '../utils.ts/StatusCodes';
 
 function errorMiddleware(err: Error, _req: Request, res: Response, next: NextFunction) {
-  const { name, message } = err;
+  const { message } = err;
 
-  switch (name) {
-    case 'ValidationError':
-      res.status(400).json({ message });
+  switch (message) {
+    case 'Username or password invalid':
+      res.status(StatusCodes.UNAUTHORIZED).json({ message });
       break;
-    case 'NotFoundError':
-      res.status(404).json({ message });
+    case '"password" is required':
+      res.status(StatusCodes.BAD_REQUEST).json({ message });
       break;
-    case 'conflictError':
-      res.status(409).json({ message });
+    case '"username" is required':
+      res.status(StatusCodes.BAD_REQUEST).json({ message });
       break;
     default:
       console.log(err);
-      res.sendStatus(500);
+      res.sendStatus(StatusCodes.UNKNOWN_ERROR);
   }
   next();
 }
