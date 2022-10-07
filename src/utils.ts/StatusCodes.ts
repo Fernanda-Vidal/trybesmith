d99f91b7b1ae5f00102c709657279cbb44876a61
Fernanda Vidal <fevidal.dev@gmail.com>
2022-10-07 19:33:27 -0300

    feat: add validation.

--- src/utils.ts/StatusCodes.ts
+++ src/utils.ts/StatusCodes.ts
@@ -1,10 +1,11 @@
 const StatusCodes = {
   OK: 200,
-  NOT_FOUND: 404,
   CREATED: 201,
+  NO_CONTENT: 204,
   BAD_REQUEST: 400,
   UNAUTHORIZED: 401,
-  NO_CONTENT: 204,
+  NOT_FOUND: 404,
+  UNPROCESSABLE_ENTITY: 422,
 };
 
 export default StatusCodes;
---
+++ src/utils.ts/validation.ts
@@ -0,0 +1,14 @@
+import StatusCodes from "./StatusCodes";
+
+export default function validation(field: string, request: string) {
+  if (!field) return { message: `"${request}" is required`, status: StatusCodes.BAD_REQUEST };
+  if (typeof field !== 'string') {
+    return ({ message: `"${request}" must be a string`, status: StatusCodes.UNPROCESSABLE_ENTITY });
+  }
+  if (field.length < 3) {
+    const err = `"${request}" length must be at least 3 characters long`;
+    return ({ message: err, status: StatusCodes.UNPROCESSABLE_ENTITY });
+  }
+
+  return false;
+}
