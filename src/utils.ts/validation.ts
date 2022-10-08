3014777a573ce59a1f094a9e6c1ebd8f5624d1f9
Fernanda Vidal <fevidal.dev@gmail.com>
2022-10-07 22:38:38 -0300

    feat: add new validations.

--- src/utils.ts/validation.ts
+++ src/utils.ts/validation.ts
@@ -1,14 +1,31 @@
 import StatusCodes from './StatusCodes';
 
-export default function validationString(field: string, request: string) {
+export function validationString(field: string, request: string, num: number) {
   if (!field) return { message: `"${request}" is required`, status: StatusCodes.BAD_REQUEST };
   
   if (typeof field !== 'string') {
     return ({ message: `"${request}" must be a string`, status: StatusCodes.UNPROCESSABLE_ENTITY });
   }
 
-  if (field.length < 3) {
-    const err = `"${request}" length must be at least 3 characters long`;
+  if (field.length < num) {
+    const err = `"${request}" length must be at least ${num} characters long`;
+    return ({ message: err, status: StatusCodes.UNPROCESSABLE_ENTITY });
+  }
+
+  return false;
+}
+
+export function validationNumber(level: number) {
+  if (!level && level !== 0) {
+    return { message: '"level" is required', status: StatusCodes.BAD_REQUEST };
+  }
+
+  if (typeof level !== 'number') {
+    return ({ message: '"level" must be a number', status: StatusCodes.UNPROCESSABLE_ENTITY });
+  }
+
+  if (level <= 0) {
+    const err = '"level" must be greater than or equal to 1';
     return ({ message: err, status: StatusCodes.UNPROCESSABLE_ENTITY });
   }
 
