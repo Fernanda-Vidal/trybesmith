ad43756063a6348f1360e48b84042c3bc7e45b04
Fernanda Vidal <fevidal.dev@gmail.com>
2022-10-08 18:41:29 -0300

    cheat: rollback to an older version.

--- src/middlewares/loginMiddleware.ts
+++ src/middlewares/loginMiddleware.ts
@@ -5,7 +5,7 @@ import StatusCodes from '../utils.ts/StatusCodes';
 function loginMiddleware(req: Request, _res: Response, next: NextFunction) {
   const { username, password } = req.body;
 
-  if (!username) next(new HttpException('"username" is required', StatusCodes.BAD_REQUEST));
+  if (!username) throw new HttpException('"username" is required', StatusCodes.BAD_REQUEST);
   if (!password) throw new HttpException('"password" is required', StatusCodes.BAD_REQUEST);
 
   next();
