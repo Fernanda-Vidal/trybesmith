bea2364b4488069622861c01b0c7f5151f7ef5d4
Fernanda Vidal <fevidal.dev@gmail.com>
2022-10-08 18:03:54 -0300

    fix: type JWT.

--- src/utils.ts/JWT.ts
+++ src/utils.ts/JWT.ts
@@ -1,15 +1,24 @@
 import dotenv from 'dotenv';
-import jwt from 'jsonwebtoken';
+import jwt, { SignOptions, Secret, JwtPayload } from 'jsonwebtoken';
 import IUser from '../interfaces/user.interface';
 
 dotenv.config();
+const PASSWORD = process.env.JWT_SECRET;
 
-const generateToken = (user: IUser): string => {
-  const { id, username, classe, level } = user;
-  const password = process.env.JWT_SECRET;
-  const jwtConfig = { expiresIn: '1h' };
-  const token = jwt.sign({ id, username, classe, level }, password as string, jwtConfig);
+const generateToken = (user: Omit<IUser, 'classe' | 'level' | 'password'>): string => {
+  const { id, username } = user;
+  const payload = { id, username };
+  const jwtConfig = { expiresIn: '1h', algorithm: 'HS256' };
+  const token = jwt
+    .sign(payload, PASSWORD as Secret, jwtConfig as SignOptions);
+
   return token;
+};
+
+export const validateToken = (token: string): JwtPayload => {
+  const validate = jwt.verify(token, PASSWORD as Secret);
+
+  return validate as JwtPayload;
 };
 
 export default generateToken;
